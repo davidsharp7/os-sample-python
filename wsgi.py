20993b0496d218e61a9db6f1477b273eb3df3004
sharpd <number6labs@gmail.com>
2021-01-01 16:12:19 +1300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!! DAVIDddd ssssRsssULEaadddaaS!"
+    return "This was deployed automatically"
 
 if __name__ == "__main__":
     application.run()
