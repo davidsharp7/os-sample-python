6b42807cbc999e81d91f422f9e08768fa6139c45
sharpd <number6labs@gmail.com>
2021-01-02 08:33:56 +1300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "with a new stage"
+    return "with a new stage - this is cool"
 
 if __name__ == "__main__":
     application.run()
