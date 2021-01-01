12d8d660e94f791effe98856438ea9e7e99e9e74
davidsharp7 <34074888+davidsharp7@users.noreply.github.com>
2021-01-01 16:26:49 +1300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "This was deployed automatically"
+    return "This was deployed automatically!!"
 
 if __name__ == "__main__":
     application.run()
